c2289d2b6ea5c7c75ea5abf344e41c61471bc203
Luciano Ciccariello <xeeynamo@hotmail.com>
2017-08-11 11:31:03 +0200

    Now it's possible to cancel the removal of a file or folder from files tree

--- Xe.Tools.GameStudio/ResourceView.xaml.cs
+++ Xe.Tools.GameStudio/ResourceView.xaml.cs
@@ -143,8 +143,8 @@ namespace Xe.Tools.GameStudio
             var node = _resourceManager.SelectedNode;
             if (node == null) return;
 
-            const string strDeleteDir = "You are deleting a folder with some files inside.\nDo you want to delete the content? If you click No, they will be deleted only from this application.";
-            const string strFileDir = "Do you want to delete the file? If you click No, it will be deleted only from this application.";
+            const string strDeleteDir = "You are removing a folder with some files inside.\nDo you want to delete the content? If you click No, they will be removed only by the project.";
+            const string strFileDir = "Do you want to delete the file? If you click No, it will be removed only by the project.";
 
             string strMessage;
             var path = _resourceManager.SelectedFullPath;
@@ -158,16 +158,31 @@ namespace Xe.Tools.GameStudio
             else
                 strMessage = strFileDir;
 
-            bool physicalDelete;
+            bool? physicalDelete;
             if (strMessage != null)
             {
-                var r = MessageBox.Show(strMessage, "Delete confirmation",
-                    MessageBoxButton.YesNo, MessageBoxImage.Warning);
-                physicalDelete = r == MessageBoxResult.Yes;
+                switch(MessageBox.Show(strMessage, "Delete confirmation",
+                    MessageBoxButton.YesNoCancel, MessageBoxImage.Warning))
+                {
+                    case MessageBoxResult.Yes:
+                    case MessageBoxResult.OK:
+                        physicalDelete = true;
+                        break;
+                    case MessageBoxResult.No:
+                        physicalDelete = false;
+                        break;
+                    default:
+                        physicalDelete = null;
+                        break;
+                }
             }
             else
                 physicalDelete = true;
-            _resourceManager.Delete(physicalDelete);
+
+            if (physicalDelete.HasValue)
+            {
+                _resourceManager.Delete(physicalDelete.Value);
+            }
         }
 
         private void ctrlButtonAddFolder_Click(object sender, RoutedEventArgs e)
