cd8128ee0cd69a84a4ea8b0127f54f1ff479e757
Luciano Ciccariello <xeeynamo@hotmail.com>
2018-06-12 11:56:34 +0200

    Alignment fixes during Kernel exportation

--- Xe.Tools.Modules.Kernel/Kernel.Actor.cs
+++ Xe.Tools.Modules.Kernel/Kernel.Actor.cs
@@ -26,6 +26,9 @@ namespace Xe.Tools.Modules.Kernel
 				w.Write(item.Description.GetXeHash());
 				w.Write(animIndex);
 
+				w.Write((byte)item.Level);
+				w.Write((byte)0);
+				w.Write((byte)0);
 				w.Write((byte)0);
 				w.Write(item.Experience);
 				w.Write((short)item.Health);
@@ -36,9 +39,12 @@ namespace Xe.Tools.Modules.Kernel
 				w.Write((byte)item.Defense);
 				w.Write((byte)item.AttackSpecial);
 				w.Write((byte)item.DefenseSpecial);
+				w.Write(0);
 
 				w.Write((byte)item.Drop.Hp);
 				w.Write((byte)item.Drop.Mp);
+				w.Write((byte)0);
+				w.Write((byte)0);
 				w.Write((short)item.Drop.Exp);
 				w.Write((short)item.Drop.Money);
 
@@ -48,7 +54,7 @@ namespace Xe.Tools.Modules.Kernel
 
 			Table["ActorsAnimations"] = anims;
 
-			return "Status1";
+			return "Actor1";
 		}
 
 		private static void WriteDropItem(BinaryWriter w, KernelData k, DropItem dropItem)
@@ -59,11 +65,13 @@ namespace Xe.Tools.Modules.Kernel
 				var id = item?.GetXeHash() ?? 0;
 
 				w.Write(id);
-				w.Write((short)dropItem.Count);
-				w.Write((short)dropItem.Rarity);
+				w.Write((byte)dropItem.Count);
+				w.Write((byte)dropItem.Rarity);
+				w.Write((short)0);
 			}
 			else
 			{
+				w.Write(0);
 				w.Write(0);
 			}
 		}
